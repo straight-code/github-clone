9c09e5c88e042baaa6830c564d94b434a96dab9a
straight-code <97550737+straight-code@users.noreply.github.com>
2022-03-05 23:21:44 +0000

    v0.0.1

---
+++ build/precache-manifest.d8c55dc3bfbaf3a9e978e88216b80a4b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6a752b9a64ba4010caaf87322d7dbb13",
+    "url": "/index.html"
+  },
+  {
+    "revision": "24a2354f043ffe4bd9cd",
+    "url": "/static/css/main.352e2c49.chunk.css"
+  },
+  {
+    "revision": "ad11474284291747cca7",
+    "url": "/static/js/2.f637e95d.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/static/js/2.f637e95d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "24a2354f043ffe4bd9cd",
+    "url": "/static/js/main.e430801c.chunk.js"
+  },
+  {
+    "revision": "d64d8ca60c569a06da1d",
+    "url": "/static/js/runtime-main.9d5340e2.js"
+  }
+]);
